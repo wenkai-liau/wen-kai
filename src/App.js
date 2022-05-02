9aba246fbab92f00d1da1648cbc9bed61e8a8218
wenkai-liau <58283836+wenkai-liau@users.noreply.github.com>
2022-05-02 12:13:41 +0800

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { createTheme, Grid, makeStyles, ThemeProvider, responsiveFontSizes } from '@material-ui/core';
+import { createTheme, Grid, makeStyles, ThemeProvider, responsiveFontSizes, Typography } from '@material-ui/core';
 import HeaderBar from './components/header';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -9,6 +9,8 @@ import {
 import About from './components/about';
 import FooterBar from './components/footerBar';
 import Books from './components/books';
+import pikachu from './images/pikachu_meme.png';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,12 +26,42 @@ const App = () =>  {
   const classes = useStyles();
   const [tabValue, setTabValue] = useState(0);
 
+  useEffect(() => {
+    const currentPath = window.location.pathname
+    const tabString = currentPath.split("/")
+    switch (tabString.at(-1)){
+      case 'cp':
+        setTabValue(1)
+        return
+      case 'books':
+        setTabValue(2)
+        return
+      case 'contact':
+        setTabValue(3)
+        return
+      default:
+        setTabValue(0)
+        return
+    }
+  }, []);
+
   const handleTabChange = (e, newValue) => {
     setTabValue(newValue);
   };
 
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
+
+  const wip = () => {
+    return (
+      <Grid container justifyContent="center" alignItems="center" style={{flexDirection: 'column', height: '85vh', width: '100%'}}>
+        <img item src={pikachu} width={385} height={314} style={{alignSelf: 'center'}}/>
+        <Typography item variant="h3">
+          Work in Progress
+        </Typography>
+      </Grid>
+    )
+  }
 
   return (
     <BrowserRouter basename="/wenkai-liau">
@@ -41,9 +73,9 @@ const App = () =>  {
           <Grid className={classes.contentContainer}>
             <Routes>
               <Route exact path={''} element={<About/>} />
-              <Route exact path={'/cp'} element={<div>Tab 2</div>} />
+              <Route exact path={'/cp'} element={wip()} />
               <Route exact path={'/books'} element={<Books/>} />
-              <Route exact path={'/contact'} element={<div>Tab 4</div>} />
+              <Route exact path={'/contact'} element={wip()} />
             </Routes>
           </Grid>
 
