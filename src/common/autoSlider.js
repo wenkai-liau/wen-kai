365696c379f83899dbb2c810132ae4d2771d7fd0
wenkai-liau <58283836+wenkai-liau@users.noreply.github.com>
2022-12-01 22:20:09 +0800

    replaced photos with japan photos

--- src/common/autoSlider.js
+++ src/common/autoSlider.js
@@ -3,11 +3,21 @@ import "./slider-styles.css";
 import AwesomeSlider from "react-awesome-slider";
 import withAutoplay from "react-awesome-slider/dist/autoplay";
 
-import imageOne from "../images/night_panorama.jpg";
-import imageTwo from "../images/craterLake.jpg";
-import imageThree from "../images/banff.jpg";
-import imageFour from "../images/marina.jpg";
-import imageFive from "../images/moraineLake.jpg";
+// import imageOne from "../images/night_panorama.jpg";
+// import imageTwo from "../images/craterLake.jpg";
+// import imageThree from "../images/banff.jpg";
+// import imageFour from "../images/marina.jpg";
+// import imageFive from "../images/moraineLake.jpg";
+
+import imageOne from "../images/chureitoPagoda.jpg";
+import imageTwoA from "../images/kawaguchiko.jpg";
+import imageTwoB from "../images/kawaguchiko2.jpg";
+import imageTwoC from "../images/kawaguchiko3.jpg";
+import imageTwoD from "../images/kawaguchiko4.jpg";
+import imageThree from "../images/chuzenjiLake.jpg";
+import imageFour from "../images/sensojiTemple.jpg";
+import imageFive from "../images/shibuyaSky.jpg";
+import imageSix from "../images/tokyoStation.jpg";
 
 const AutoSlider = ({ height, width }) => {
   const smallScreen = width < 720;
@@ -23,35 +33,72 @@ const AutoSlider = ({ height, width }) => {
       <div className="row">
         <img
           src={imageOne}
-          width={smallScreen ? width : 1100}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
           height={smallScreen ? 150 : 350}
         />
       </div>
       <div className="row">
         <img
-          src={imageTwo}
-          width={smallScreen ? width : 1100}
+          src={imageTwoA}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
+          height={smallScreen ? 150 : 350}
+        />
+      </div>
+      <div className="row">
+        <img
+          src={imageTwoB}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
+          height={smallScreen ? 150 : 350}
+        />
+      </div>
+      <div className="row">
+        <img
+          src={imageTwoC}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
+          height={smallScreen ? 150 : 350}
+        />
+      </div>
+      <div className="row">
+        <img
+          src={imageTwoD}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
           height={smallScreen ? 150 : 350}
         />
       </div>
       <div className="row">
         <img
           src={imageThree}
-          width={smallScreen ? width : 1100}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
           height={smallScreen ? 150 : 350}
         />
       </div>
       <div className="row">
         <img
           src={imageFour}
-          width={smallScreen ? width : 1100}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
           height={smallScreen ? 150 : 350}
         />
       </div>
       <div className="row">
         <img
           src={imageFive}
-          width={smallScreen ? width : 1100}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
+          height={smallScreen ? 150 : 350}
+        />
+      </div>
+      <div className="row">
+        <img
+          src={imageSix}
+          // width={smallScreen ? width : 1100}
+          max-width={"100%"}
           height={smallScreen ? 150 : 350}
         />
       </div>
